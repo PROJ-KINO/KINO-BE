aae28cbd8ade161215c332ae38ef9d7a55f4cd4e
Jungsang Gi <wjdtkdrl15432@gmail.com>
2025-07-16 09:36:33 +0900

    fix : remove duplication repository

--- src/main/java/com/hamss2/KINO/api/admin/service/AdminService.java
+++ src/main/java/com/hamss2/KINO/api/admin/service/AdminService.java
@@ -2,8 +2,11 @@ package com.hamss2.KINO.api.admin.service;
 
 import com.hamss2.KINO.api.admin.dto.*;
 import com.hamss2.KINO.api.admin.repository.*;
-import com.hamss2.KINO.api.home.repository.*;
+import com.hamss2.KINO.api.home.repository.ReviewRepository;
+import com.hamss2.KINO.api.movieDetail.repository.*;
 import com.hamss2.KINO.api.entity.*;
+import com.hamss2.KINO.api.movieDetail.repository.ReportRepository;
+import com.hamss2.KINO.api.movieDetail.repository.ShortReviewRepository;
 import com.hamss2.KINO.api.testPackage.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
