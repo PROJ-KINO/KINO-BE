f86de475918a040306e521c0e5551496deb71a21
parkjiwon <ji11won@naver.com>
2025-07-17 03:40:55 +0900

    Feat: 토큰 정보 저장

---
+++ src/main/java/com/hamss2/KINO/api/auth/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package com.hamss2.KINO.api.auth.repository;
+
+import com.hamss2.KINO.api.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<RefreshToken, Long> {
+
+}
--- src/main/java/com/hamss2/KINO/api/entity/RefreshToken.java
+++ src/main/java/com/hamss2/KINO/api/entity/RefreshToken.java
@@ -1,10 +1,17 @@
 package com.hamss2.KINO.api.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.extern.slf4j.Slf4j;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
@@ -13,6 +20,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Entity
 public class RefreshToken {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,6 +30,12 @@ public class RefreshToken {
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    public static RefreshToken create(String token) {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(token);
+        return refreshToken;
+    }
 
     @PrePersist
     private void prePersist() {
