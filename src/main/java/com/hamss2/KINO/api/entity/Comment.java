eaf6b4d306fc087db5082fdb01b55c8d7ac50194
parkjiwon <ji11won@naver.com>
2025-07-17 22:42:51 +0900

    Feat: Comment 기본값 지정

--- src/main/java/com/hamss2/KINO/api/entity/Comment.java
+++ src/main/java/com/hamss2/KINO/api/entity/Comment.java
@@ -1,12 +1,22 @@
 package com.hamss2.KINO.api.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.ColumnDefault;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,6 +25,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Slf4j
 public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
@@ -24,11 +35,11 @@ public class Comment {
 
     @Column(nullable = false)
     @ColumnDefault("false")
-    private Boolean isDeleted;
+    private Boolean isDeleted = false;
 
     @Column(nullable = false)
     @ColumnDefault("true")
-    private Boolean isActive;
+    private Boolean isActive = true;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
