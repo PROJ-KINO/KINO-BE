3f79bec98152259875c38c211a1933286278b677
Jungsang Gi <wjdtkdrl15432@gmail.com>
2025-07-18 09:22:38 +0900

    fix : res dto, remove comments likes

--- src/main/java/com/hamss2/KINO/api/entity/Comment.java
+++ src/main/java/com/hamss2/KINO/api/entity/Comment.java
@@ -23,9 +23,6 @@ public class Comment {
     private String content;
 
     @Column(nullable = false)
-    private int likes = 0;
-
-    @Column(nullable = false)
     @ColumnDefault("false")
     private Boolean isDeleted;
 
