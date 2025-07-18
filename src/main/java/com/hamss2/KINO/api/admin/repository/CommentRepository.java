2dc3762c114b8171d8ad54ba5658012300bb82bd
parkjiwon <ji11won@naver.com>
2025-07-18 10:49:12 +0900

    Feat: 댓글 신고하기 기능

--- src/main/java/com/hamss2/KINO/api/admin/repository/CommentRepository.java
+++ src/main/java/com/hamss2/KINO/api/admin/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.hamss2.KINO.api.admin.repository;
 
 import com.hamss2.KINO.api.entity.Comment;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 리뷰 ID로 댓글 조회
     List<Comment> findByReviewReviewId(Long reviewId);
 
+    Optional<Comment> findByCommentId(Long commentId);
 }
--- src/main/java/com/hamss2/KINO/api/home/repository/ReviewRepository.java
+++ src/main/java/com/hamss2/KINO/api/home/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.hamss2.KINO.api.home.repository;
 import com.hamss2.KINO.api.entity.Movie;
 import com.hamss2.KINO.api.entity.Review;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,13 +13,14 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     // 좋아요순 TOP 10 리뷰
     @Query("""
-    SELECT r FROM Review r
-    LEFT JOIN r.reviewLikes rl
-    WHERE r.isDeleted = false
-    GROUP BY r
-    ORDER BY COUNT(rl) DESC, r.createdAt DESC
-""")
+            SELECT r FROM Review r
+            LEFT JOIN r.reviewLikes rl
+            WHERE r.isDeleted = false
+            GROUP BY r
+            ORDER BY COUNT(rl) DESC, r.createdAt DESC
+        """)
     List<Review> findTop10ByReviewLikes(Pageable pageable);
+
     // 영화별 리뷰 리스트
     Page<Review> findAllByMovieAndIsDeletedFalseOrderByCreatedAtDesc(Movie movie,
         Pageable pageable);
@@ -27,4 +29,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     long countByMovieAndIsDeletedFalse(Movie movie);
 
     Page<Review> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    Optional<Review> findByReviewId(Long reviewId);
 }
