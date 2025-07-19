917be81da428ef51968a2cdcecdbb60592e004f1
see101702 <see101702@gmail.com>
2025-07-20 00:01:40 +0900

    #4 teaserDto에도 정보 추가 영화 검색 lazy로딩 해결

--- src/main/java/com/hamss2/KINO/api/home/dto/res/TeaserDto.java
+++ src/main/java/com/hamss2/KINO/api/home/dto/res/TeaserDto.java
@@ -3,6 +3,9 @@ package com.hamss2.KINO.api.home.dto.res;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Getter
 @Setter
 public class TeaserDto {
@@ -11,4 +14,7 @@ public class TeaserDto {
     private String teaserUrl;
     private String plot;
     private String stillCutUrl;
+    private LocalDate releaseDate;
+    private int runningTime;
+    private List<String> genres;
 }
