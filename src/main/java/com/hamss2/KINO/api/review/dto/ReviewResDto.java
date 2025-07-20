6e73c204de705af3c9f4e08f5b53f7ec31e88302
parkjiwon <ji11won@naver.com>
2025-07-20 20:52:59 +0900

    Feat: 리뷰 게시글 반환값 변경

--- src/main/java/com/hamss2/KINO/api/review/dto/ReviewResDto.java
+++ src/main/java/com/hamss2/KINO/api/review/dto/ReviewResDto.java
@@ -18,9 +18,9 @@ public class ReviewResDto {
     private Integer likeCount;
     private Integer commentCount;
 
-    private Long writerId;
-    private String writerNickname;
-    private String writerImage;
+    private Long userId;
+    private String userNickname;
+    private String userImage;
 
     private Boolean isMine;
     private Boolean isHeart;
